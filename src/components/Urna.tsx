d642b40182e292295cdad14ba7b4659ac9aed73d
Pedro Victor <pedro20002victor@gmail.com>
2022-09-17 14:22:20 -0300

    Verificar voto

--- src/components/Urna.tsx
+++ src/components/Urna.tsx
@@ -12,76 +12,76 @@ import {
 } from "../style/UI";
 
 export function UrnaEletronica() {
-  const [numeroCandidato, setnumeroCandidato] = useState<number[]>([]);
-  const [ candidato, setCandidato ] = useState([
+  const [numeroCandidato, setnumeroCandidato] = useState<string[]>([]);
+  const [candidato, setCandidato] = useState([
     {
-      id: 1, 
+      id: 1,
       numero: "22",
-      name: 'Bolsonaro',
-      qtdVotos: 0
+      name: "Bolsonaro",
+      qtdVotos: 0,
     },
     {
       id: 2,
-      numero: "13", 
-      name: 'Lula',
-      qtdVotos: 0
+      numero: "13",
+      name: "Lula",
+      qtdVotos: 0,
     },
     {
       id: 3,
       numero: "12",
-      name: 'Ciro da Massa',
-      qtdVotos: 0
-    }
-  ])
-
-  // Criar estado para gerenciar estes numeros | Olhar repo da calculadora online para melhor exemplo
-
-  const getValues = (item: number) => {
-    console.log(item, typeof item);
-
+      name: "Ciro da Massa",
+      qtdVotos: 0,
+    },
+  ]);
+  const keyValue = (item: string) => {
     if (numeroCandidato.length < 2) {
       setnumeroCandidato([...numeroCandidato, item]);
     } else {
       setnumeroCandidato([]);
       console.log("Numero maximo de caracteres atingido");
     }
-
-    console.log(numeroCandidato);
   };
 
   const confirm = () => {
-    if(numeroCandidato.length == 2) {
+    if (numeroCandidato.length == 2) {
+      // Retorna o numero digitado pelo usuario
+      let userChoice = numeroCandidato.concat(numeroCandidato[0] + numeroCandidato[1])[2];
 
-      let c = numeroCandidato.concat(numeroCandidato[0] + numeroCandidato[1]);
-      console.log(c);
-      // const candTemp = candidato.map((item) => item.numero == item.numero)
+      const filterCandidato = candidato.filter(
+        (item) => item.numero == userChoice
+      );
+
+      const nPresidente = filterCandidato[0]
+      console.log(nPresidente);
+
+      // if(userChoice == nPresidente.numero) {
+      //   console.log(`Voto adicionado ao presidente ${nPresidente.name}`)
+      // } else {
+      //   console.log('Voto em nulo')
+      // }
 
       setnumeroCandidato([]);
     } else {
-      console.log('Voto em branco');
+      console.log("Voto em branco");
     }
   };
 
   return (
     <Container>
       <UrnaContainer>
-        <UrnaDisplay>
-          {
-            <UrnaNumber>{numeroCandidato}</UrnaNumber>
-          }
-        </UrnaDisplay>
+        <UrnaDisplay>{<UrnaNumber>{numeroCandidato}</UrnaNumber>}</UrnaDisplay>
         <div className="keys">
           <div className="all-numbers">
             <div className="btn-number-grid">
-              <ButtonNumber onClick={() => getValues(1)}>1</ButtonNumber>
-              <ButtonNumber onClick={() => getValues(2)}>2</ButtonNumber>
-              <ButtonNumber onClick={() => getValues(3)}>3</ButtonNumber>
-              <ButtonNumber onClick={() => getValues(4)}>4</ButtonNumber>
-              <ButtonNumber onClick={() => getValues(5)}>5</ButtonNumber>
-              <ButtonNumber onClick={() => getValues(6)}>6</ButtonNumber>
-              <ButtonNumber onClick={() => getValues(7)}>7</ButtonNumber>
-              <ButtonNumber onClick={() => getValues(8)}>8</ButtonNumber>
-              <ButtonNumber onClick={() => getValues(9)}>9</ButtonNumber>
+              <ButtonNumber onClick={() => keyValue("1")}>1</ButtonNumber>
+              <ButtonNumber onClick={() => keyValue("2")}>2</ButtonNumber>
+              <ButtonNumber onClick={() => keyValue("3")}>3</ButtonNumber>
+              <ButtonNumber onClick={() => keyValue("4")}>4</ButtonNumber>
+              <ButtonNumber onClick={() => keyValue("5")}>5</ButtonNumber>
+              <ButtonNumber onClick={() => keyValue("6")}>6</ButtonNumber>
+              <ButtonNumber onClick={() => keyValue("7")}>7</ButtonNumber>
+              <ButtonNumber onClick={() => keyValue("8")}>8</ButtonNumber>
+              <ButtonNumber onClick={() => keyValue("9")}>9</ButtonNumber>
             </div>
             <div className="btn-0">
               <ButtonNumber>0</ButtonNumber>
